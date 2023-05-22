1d22bebccf5e1dcee78fe278fb01db2468ed8995
Andy Rothwell <Andy.Rothwell@phila.gov>
2023-05-22 09:35:29 -0400

    makes some function names clearer, adds comments explaining code

--- time-parser.py
+++ time-parser.py
@@ -15,34 +15,38 @@ meals = pd.read_csv(INPUT_FILE)
 
 days_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-def checkNan(value):
+# function to check whether value is NaN
+def checkForNan(value):
     if pd.isna(value):
         return True
     else:
         return False
 
-def checkAMPM(value):
+# function to check whether value includes any version of AM or PM
+def checkForAMPM(value):
     allValues = ['AM', 'PM', 'am', 'pm', 'a.m.', 'p.m.', 'A.M.', 'P.M.']
-    if checkNan(value) == False:
+    if checkForNan(value) == False:
         if any(x in value for x in allValues):
             return True
         else:
             return False
     else:
         return False
-    
-def checkPM(value):
+
+# function to check whether value includes any version of PM
+def checkForPM(value):
     allValues = ['PM', 'pm', 'p.m.', 'P.M.']
-    if checkNan(value) == False:
+    if checkForNan(value) == False:
         if any(x in value for x in allValues):
             return True
         else:
             return False
     else:
         return False
-    
-def checkColon(value):
-    if checkNan(value) == False:
+
+# function to check whether value includes a colon
+def checkForColon(value):
+    if checkForNan(value) == False:
         if ':' in value:
             return True
         else:
@@ -50,23 +54,10 @@ def checkColon(value):
     else:
         return False
 
-def hasAMPMAndColon(time):
-    print('hasAMPMAndColon, time: ', time)
-    hasPM = checkPM(time.strip())
-    timeBeforeColon = time.split(':')[0].strip()
-    timeAfterColonWithAMPM = time.split(':')[1]
-    timeAfterColon = re.split('a|A|p|P', timeAfterColonWithAMPM)[0].strip()
-    if (hasPM):
-        if int(timeBeforeColon) == 12:
-            return '12:' + timeAfterColon
-        else:
-            return str(int(timeBeforeColon) + 12) + ':' + timeAfterColon
-    else:
-        return timeBeforeColon + ':' + timeAfterColon
-    
-def hasEndAMPMAndColon(time, endTime):
-    print('hasEndAMPMAndColon, time: ', time)
-    hasPM = checkPM(endTime.strip())
+# function to return single time if the value includes a colon and AM or PM
+def hasColonAndAMPM(time):
+    print('hasColonAndAMPM, time: ', time)
+    hasPM = checkForPM(time.strip())
     timeBeforeColon = time.split(':')[0].strip()
     timeAfterColonWithAMPM = time.split(':')[1]
     timeAfterColon = re.split('a|A|p|P', timeAfterColonWithAMPM)[0].strip()
@@ -78,8 +69,24 @@ def hasEndAMPMAndColon(time, endTime):
     else:
         return timeBeforeColon + ':' + timeAfterColon
 
-def hasAMPMAndNoColon(time):
-    hasPM = checkPM(time)
+# function to return single time if the value includes a colon, the start time does NOT include AM or PM, and the end time includes AM or PM
+def hasColonAndEndAMPM(time, endTime):
+    print('hasColonAndEndAMPM, time: ', time)
+    hasPM = checkForPM(endTime.strip())
+    timeBeforeColon = time.split(':')[0].strip()
+    timeAfterColonWithAMPM = time.split(':')[1]
+    timeAfterColon = re.split('a|A|p|P', timeAfterColonWithAMPM)[0].strip()
+    if (hasPM):
+        if int(timeBeforeColon) == 12:
+            return '12:' + timeAfterColon
+        else:
+            return str(int(timeBeforeColon) + 12) + ':' + timeAfterColon
+    else:
+        return timeBeforeColon + ':' + timeAfterColon
+
+# function to return single time if the value does NOT include a colon and DOES include AM or PM
+def hasNoColonAndAMPM(time):
+    hasPM = checkForPM(time)
     if (hasPM):
         if int(re.split('p|P', time)[0].strip()) == 12:
             return '12:00'
@@ -87,9 +94,10 @@ def hasAMPMAndNoColon(time):
             return str(int(re.split('p|P', time)[0].strip()) + 12) + ':00'
     else:
         return re.split('a|A', time)[0].strip() + ':00'
-    
-def hasEndAMPMAndNoColon(time, endTime):
-    hasEndPM = checkPM(endTime)
+
+# function to return single time if the value does NOT include a colon, the start time does NOT include AM or PM, and the end time includes AM or PM
+def hasNoColonAndEndAMPM(time, endTime):
+    hasEndPM = checkForPM(endTime)
     if (hasEndPM):
         if int(re.split('p|P', time)[0].strip()) == 12:
             return '12:00'
@@ -98,63 +106,65 @@ def hasEndAMPMAndNoColon(time, endTime):
     else:
         return re.split('a|A', time)[0].strip() + ':00'
 
-def checkAll(time):
+# function that takes a string that includes start and end time, and returns a list of the parsed start and end times
+def parseMealTimes(stringTime):
     parsedTimes = pd.Series()
-    isNan = checkNan(time)
+    isNan = checkForNan(stringTime)
 
     if (isNan):
         parsedTimes[0] = ''
         parsedTimes[1] = ''
     else:
-        startAndEnd = time.split('-')
-        startHasAMPM = checkAMPM(startAndEnd[0])
-        startHasColon = checkColon(startAndEnd[0])
-        endHasAMPM = checkAMPM(startAndEnd[1])
-        endHasColon = checkColon(startAndEnd[1])
+        startAndEndTimes = stringTime.split('-')
+        startHasAMPM = checkForAMPM(startAndEndTimes[0])
+        startHasColon = checkForColon(startAndEndTimes[0])
+        endHasAMPM = checkForAMPM(startAndEndTimes[1])
+        endHasColon = checkForColon(startAndEndTimes[1])
 
         if (startHasAMPM and startHasColon):
-            parsedTimes[0] = hasAMPMAndColon(startAndEnd[0])
+            parsedTimes[0] = hasColonAndAMPM(startAndEndTimes[0])
         elif (startHasAMPM and startHasColon == False):
-            parsedTimes[0] = hasAMPMAndNoColon(startAndEnd[0])
+            parsedTimes[0] = hasNoColonAndAMPM(startAndEndTimes[0])
         elif (startHasAMPM == False and endHasAMPM and startHasColon):
-            parsedTimes[0] = hasEndAMPMAndColon(startAndEnd[0], startAndEnd[1])
+            parsedTimes[0] = hasColonAndEndAMPM(startAndEndTimes[0], startAndEndTimes[1])
         elif (startHasAMPM == False and endHasAMPM and startHasColon == False):
-            parsedTimes[0] = hasEndAMPMAndNoColon(startAndEnd[0], startAndEnd[1])
+            parsedTimes[0] = hasNoColonAndEndAMPM(startAndEndTimes[0], startAndEndTimes[1])
         else:
-            parsedTimes[0] = startAndEnd[0]
+            parsedTimes[0] = startAndEndTimes[0]
         
         if (endHasAMPM and endHasColon):
-            parsedTimes[1] = hasAMPMAndColon(startAndEnd[1])
+            parsedTimes[1] = hasColonAndAMPM(startAndEndTimes[1])
         elif (endHasAMPM and endHasColon == False):
-            parsedTimes[1] = hasAMPMAndNoColon(startAndEnd[1])
+            parsedTimes[1] = hasNoColonAndAMPM(startAndEndTimes[1])
         else:
-            parsedTimes[1] = startAndEnd[1]
+            parsedTimes[1] = startAndEndTimes[1]
 
     return parsedTimes
 
+# loop through each day of the week
 for day in days_list:
+    # since some sites have two meals, split the meals into two columns
     meals_both = meals[day].str.split('&', n=1, expand=True)
 
-    # meals_first = meals_both[0].str.split('-', n=1, expand=True)
-    # meals_second = meals_both[1].str.split('-', n=1, expand=True)
-
+    # parse the start and end times for the first (or only) meal
     meals_first_parsed = pd.Series()    
-    for count, time in enumerate(meals_both[0]):
-        meals_first_parsed[count] = checkAll(time)
+    for count, stringTime in enumerate(meals_both[0]):
+        meals_first_parsed[count] = parseMealTimes(stringTime)
     meals_first_parsed_start = pd.Series()
     meals_first_parsed_end = pd.Series()
-    for count, time in enumerate(meals_first_parsed):
-        meals_first_parsed_start[count] = time[0]
-        meals_first_parsed_end[count] = time[1]
+    for count, parsedTime in enumerate(meals_first_parsed):
+        meals_first_parsed_start[count] = parsedTime[0]
+        meals_first_parsed_end[count] = parsedTime[1]
 
+    # parse the start and end times for the second meal
     meals_second_parsed = pd.Series()    
-    for count, time in enumerate(meals_both[1]):
-        meals_second_parsed[count] = checkAll(time)
+    for count, stringTime in enumerate(meals_both[1]):
+        meals_second_parsed[count] = parseMealTimes(stringTime)
     meals_second_parsed_start = pd.Series()
     meals_second_parsed_end = pd.Series()
-    for count, time in enumerate(meals_second_parsed):
-        meals_second_parsed_start[count] = time[0]
-        meals_second_parsed_end[count] = time[1]
+    for count, parsedTime in enumerate(meals_second_parsed):
+        meals_second_parsed_start[count] = parsedTime[0]
+        meals_second_parsed_end[count] = parsedTime[1]
 
     meals[day+'_start1']=meals_first_parsed_start
     meals[day+'_end1']=meals_first_parsed_end
